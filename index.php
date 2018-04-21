6220868d1d82c0c1cef6028712ece142381eb987
Fabricio Ferreira <fabfband@gmail.com>
2018-04-21 11:00:20 -0300

    teste7

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-    echo "Hello World";
-<<<<<<< HEAD
-    echo "teste6";
-
+    echo "Teste7";
 ?>
-=======
-    echo "teste4";
-    echo "teste";
-    ?>
->>>>>>> ecb55b13e21c75c37cd3e0ab805680196da37114
